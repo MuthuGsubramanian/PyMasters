3e2df6d5b7a5f15ba25096a5f7793e019d2c15f0
muthug88 <muthu.g.subramanian@gmail.com>
2024-05-24 23:22:07 +0530

    page design

--- Streamlit/streamlitApp.py
+++ Streamlit/streamlitApp.py
@@ -5,6 +5,17 @@ warnings.filterwarnings('ignore')
 
 st.set_page_config(page_title="Pymasters", layout="wide")
 
-st.title("PyMasters Home")
+st.title("PyMasters")
 
 st.sidebar.image('https://i.pinimg.com/originals/92/60/dd/9260dd459aa4566cfa25e86a3f10ea1b.png')
+
+col1, col2, col3 = st.columns(3)
+with col1:
+    st.markdown('The Python Way')
+    st.text_area('This is a place to explore and develop oppertunities with python')
+with col2:
+    st.markdown('Python Projects')
+    st.text_area('This area is to analyze the python projects completed')
+with col3:
+    st.markdown('Future Projects')
+    st.text_area('This area is to discuss on the future projects')
