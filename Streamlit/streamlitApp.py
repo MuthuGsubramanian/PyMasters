09e96121019e3e0b6bcb848440b9c4acd435f24b
muthug88 <muthu.g.subramanian@gmail.com>
2024-05-24 22:08:10 +0530

    initial commit

--- Streamlit/streamlitApp.py
+++ Streamlit/streamlitApp.py
@@ -6,4 +6,5 @@ warnings.filterwarnings('ignore')
 st.set_page_config(page_title="Pymasters", layout="wide")
 
 st.title("PyMasters Home")
-st.markdown('<style>div.block-container{padding-top:1rem;}</style>', unsafe_allow_html=True)
+
+st.sidebar.image('https://i.pinimg.com/originals/92/60/dd/9260dd459aa4566cfa25e86a3f10ea1b.png')
